a977e7eb795c662c27956852bd3b677cc45b5c36
RAHUl-KAD <kadamrahulj0909@gmail.com>
2024-07-05 16:48:38 +0530

    updated the audio provider place

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,18 @@ import type { AppProps } from "next/app";
 import "../styles/globals.css";
 import ErrorBoundary from "../components/ErrorBoundry";
 import React, { lazy } from 'react';
-import { AudioProvider } from '../context/AudioContext';
-import AudioPlayer from "../components/AudioPlayerFixed";
+// import { AudioProvider } from '../context/AudioContext';
+// import AudioPlayer from "../components/AudioPlayerFixed";
 
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
       <ErrorBoundary fallback="There was an error while processing">
-        <AudioProvider>
+        {/* <AudioProvider> */}
           <Component {...pageProps} />
-          <AudioPlayer />
-        </AudioProvider>
+          {/* <AudioPlayer /> */}
+        {/* </AudioProvider> */}
       </ErrorBoundary>
     </>
   );
