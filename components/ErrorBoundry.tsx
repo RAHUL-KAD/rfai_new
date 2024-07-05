03136a2d570514f319f440988997e267a9912f83
RAHUl-KAD <kadamrahulj0909@gmail.com>
2024-07-05 15:53:14 +0530

    made some changes

--- components/ErrorBoundry.tsx
+++ components/ErrorBoundry.tsx
@@ -20,7 +20,8 @@ class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundarySta
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
-    console.log(error, errorInfo);
+    console.log("Error:", error);
+    console.log("Error Info:", errorInfo);
   }
 
   render(): ReactNode {
@@ -28,11 +29,13 @@ class ErrorBoundary extends React.Component<ErrorBoundaryProps, ErrorBoundarySta
       return (
         <div className='w-screen h-screen flex items-center justify-center flex-col space-y-5'>
           <h1 className="mx-auto max-w-4xl font-display text-2xl font-bold tracking-normal text-slate-900 sm:text-4xl mb-5 text-center">
-            Error occurred </h1>
+            Error occurred
+          </h1>
           <button
             className='items-center mt-7 justify-center font-medium rounded-xl focus-visible:outline-black focus:outline-none inline-flex bg-black border-2 border-black duration-200 focus-visible:ring-black hover:bg-transparent hover:border-black hover:text-black lg:w-auto px-6 py-3 text-center text-white'
-            onClick={() => window.location.reload()}>
-              Try Again
+            onClick={() => window.location.reload()}
+          >
+            Try Again
           </button>
         </div>
       );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,20 @@
-
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
-import ErrorBoundary from "./ErrorBoundry";
+import ErrorBoundary from "../components/ErrorBoundry";
 import React, { lazy } from 'react';
-import { useEffect } from 'react';
-import { useRouter } from 'next/router';
 import { AudioProvider } from '../context/AudioContext';
 import AudioPlayer from "../components/AudioPlayerFixed";
-
-const Footer = lazy(() => import('../components/Footer'));
 
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <>
-      <AudioProvider>
-        <ErrorBoundary fallback="There was an error while processing">
-
+      <ErrorBoundary fallback="There was an error while processing">
+        <AudioProvider>
           <Component {...pageProps} />
-          {/* <Footer /> */}
           <AudioPlayer />
-
-        </ErrorBoundary>
-      </AudioProvider>
-
+        </AudioProvider>
+      </ErrorBoundary>
     </>
   );
 }
